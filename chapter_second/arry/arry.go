dc90dd8cef67db240834f5870892d9269ee2d80c
Junya-0220 <junya010220@gmail.com>
2022-11-07 22:19:37 +0900

    配列操作

--- chapter_second/arry/arry.go
+++ chapter_second/arry/arry.go
@@ -7,7 +7,7 @@ import (
 )
 
 func ArrySum(x string) {
-		ar := strings.Split(x, " ")
+	ar := strings.Split(x, " ")
 	t := 0
 
 	for i  := 0; i < len(ar); i++ {
@@ -18,4 +18,49 @@ func ArrySum(x string) {
 		t += n
 	}
 	fmt.Println("total:", t)
+}
+
+func ArryRange(x string) {
+	ar := strings.Split(x, " ")
+	t := 0
+
+	for _ , v :=  range ar {
+		n, err := strconv.Atoi(v)
+		if err != nil {
+			fmt.Println("error")
+		}
+		t += n
+	}
+}
+// 最後に追加 変数=push(スライス,値)
+func Push(a []int, v int) []int {
+	return append(a,v)
+}
+
+// 最後を削除 変数=push(スライス)
+func Pop(a []int) []int {
+	return a[:len(a)-1]
+}
+
+//最初に追加 変数=unshift(スライス,値)
+func Unshift(a []int, v int) []int{
+	return append([]int{v},a...)
+}
+
+//最初を削除 変数=shift(スライス)
+func Shift(a []int, v int) []int {
+	return a[1:]
+}
+
+//指定の位置に追加 変数=insert(スライス,値,インデックス)
+func Insert(a []int, v int, p int) []int {
+	a = append(a,0)
+	a = append(a[:p+1], a[p:len(a)-1]...)
+	a[p] = v
+	return a
+}
+
+//指定の位置を削除 変数=remove(スライス,インデックス)
+func Remove(a []int, p int) []int {
+	return append(a[:p], a[p+1:]...)
 }
--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"handson_go/chapter_second/arry"
-	"handson_go/hello"
 )
 
 func main(){
-	x := hello.Input("input data")
-	arry.ArrySum(x)
-
+	a:= []int{10,20,30}
+	fmt.Println(a)
+	a = arry.Push(a,1000)
+	fmt.Println(a)
+	a = arry.Pop(a)
+	fmt.Println(a)
+	a = arry.Unshift(a,90)
+	fmt.Println(a)
+	a = arry.Shift(a,500)
+	fmt.Println(a)
+	a = arry.Insert(a,400,2)
+	fmt.Println(a)
+	a = arry.Remove(a,2)
+	fmt.Println(a)
 }
 
 
