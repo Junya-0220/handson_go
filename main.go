d30822808557c9a8765326ca44c7022c55015a9c
Junya-0220 <junya010220@gmail.com>
2022-11-14 23:28:50 +0900

    異なる構造体をインターフェイスでまとめる

--- main.go
+++ main.go
@@ -1,34 +1,70 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
-type Data interface {
-	Initial(name string, data[]int)
+ type Data interface {
+	SetValue(vals map[string]string)
 	PrintData()
-}
+ }
 
 type MyData struct {
 	Name string
 	Data []int
 }
 
-func(md *MyData) Initial(name string, data[]int) {
-	md.Name = name
-	md.Data = data
+func (md *MyData) SetValue(vals map[string]string) {
+	md.Name = vals["name"]
+	valt := strings.Split(vals["data"], " ")
+	vali := []int{}
+	for _, i := range valt {
+		n, _ := strconv.Atoi(i)
+		vali = append(vali,n)
+	}
+	md.Data = vali
 }
 
-func(md *MyData) PrintData() {
-	fmt.Println("Name: " , md.Name)
-	fmt.Println("Data: " , md.Data)
+func (md *MyData) PrintData() {
+	fmt.Println("Name: ", md.Name)
+	fmt.Println("Data: ", md.Data)
 }
 
-func(md *MyData) Check() {
-	fmt.Printf("Check [%s]", md.Name)
+type YourData struct {
+	Name string
+	Mail string
+	Age int
+}
+
+func (md *YourData) SetValue(vals map[string]string) {
+	md.Name = vals["name"]
+	md.Mail = vals["mail"]
+	n, _ := strconv.Atoi(vals["age"])
+	md.Age = n
+}
+
+func (md *YourData) PrintData() {
+	fmt.Printf("I'm %s. (%d).\n", md.Name,md.Age)
+	fmt.Printf("mail: %s.\n", md.Mail)
 }
 
 func main() {
-	var ob Data = new(MyData)
-	ob.Initial("Sachiko",[]int{2,3,90})
-	ob.PrintData()
-	ob.Check()
+	ob := [2]Data{}
+	ob[0] = new(MyData)
+	ob[0].SetValue(map[string]string{
+		"name": "sachiko",
+		"data": "22, 33, 44",
+	})
+	ob[1] = new(YourData)
+	ob[1].SetValue(map[string]string {
+		"name": "mami",
+		"mail": "mami@gmail.com",
+		"age": "45",
+	})
+	for _, d := range ob {
+		d.PrintData()
+		fmt.Println()
 	}
+}
