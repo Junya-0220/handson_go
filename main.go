61f0334382458dc345505adf07c8f8e0503802c9
Junya-0220 <junya010220@gmail.com>
2022-11-18 22:38:57 +0900

    並行処理

--- main.go
+++ main.go
@@ -1,51 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-type General interface{}
-
-type GData interface{
-	Set(nm string, g General) GData
-	Print()
-}
-
-type NData struct {
-	Name string
-	Data int
-}
-
-func (nd *NData) Set(nm string, g General)GData {
-	nd.Name = nm
-	nd.Data = g.(int)
-	return nd
-}
-
-func (nd *NData) Print() {
-	fmt.Printf("<<%s>> value: %d\n", nd.Name, nd.Data)
-}
-
-type SData struct {
-	Name string
-	Data string
-}
-
-func (sd *SData) Set(nm string, g General)GData {
-	sd.Name = nm
-	sd.Data = g.(string)
-	return sd
-}
-
-func (sd *SData) Print() {
-	fmt.Printf("<%s>, <%s>\n",sd.Name,sd.Data)
+func hello(s string, n int) {
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("<%d %s>", i, s)
+		time.Sleep(time.Duration(n) * time.Millisecond)
+	}
 }
 
 func main() {
-	var data = []GData{}
-	data = append(data, new(NData).Set("Taro",123))
-	data = append(data, new(SData).Set("J","hell"))
-	data = append(data, new(NData).Set("H",98777))
-	data = append(data, new(SData).Set("S","happ"))
-	for _ , ob := range data {
-		ob.Print()
-	}
+	go hello("hello", 50)
+	hello("bye", 100)
 }
