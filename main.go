7b1f87b17260361b4c08893a21509319ad7d096e
Junya-0220 <junya010220@gmail.com>
2022-11-20 23:01:38 +0900

    fyneのバージョン注意

--- main.go
+++ main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
-	"fmt"
-
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/driver/desktop"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	fmt.Println("test fyne")
-
 	a := app.New()
+	w := a.NewWindow("SysTray")
 
-	w := a.NewWindow("title") //this is title
-	w.Resize(fyne.NewSize(200,400))
+	if desk, ok := a.(desktop.App); ok {
+		m := fyne.NewMenu("MyApp",
+			fyne.NewMenuItem("Show", func() {
+				w.Show()
+			}))
+		desk.SetSystemTrayMenu(m)
+	}
 
-	w.ShowAndRun()// Finally Running our App
+	w.SetContent(widget.NewLabel("Fyne system tray"))
+	w.ShowAndRun()
 }
