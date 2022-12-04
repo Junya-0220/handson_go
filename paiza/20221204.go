9f6e15ac05b1863deed11f3881f137141799c9a4
Junya-0220 <junya010220@gmail.com>
2022-12-04 23:54:32 +0900

    paiza20221204-2

--- paiza/20221204.go
+++ paiza/20221204.go
@@ -5,41 +5,28 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
-
 
 func paiza() {
 	sc := bufio.NewScanner(os.Stdin)
     var t []string
     var m []string
-    var b bool
-
+    
 	for sc.Scan(){
 	    str := sc.Text()
         stringSplited := strings.Split(str, " ")
         t = append(t,stringSplited[0])
         m = append(m,stringSplited[1])
 	}
-	
-	if(t[0] == t[1] && m[0] < m[1]) {
-        b = false
-    }
-    if(t[0] < t[1] ){
-        b = false
-    }
-    if(t[0] == t[1] && m[0] == m[1]){
-        b = true
-    }
-    if(t[0] == t[1] && m[0] > m[1]) {
-        b = true
-    }
-    if(t[0] > t[1] ){
-        b = true
+	tf1 := fmt.Sprintf("2020-11-01T%s:%s:00+09:00",t[0],m[0])
+	tf2 := fmt.Sprintf("2020-11-01T%s:%s:00+09:00",t[1],m[1])
+	t1, _ := time.Parse(time.RFC3339, tf1)
+	t2, _ := time.Parse(time.RFC3339, tf2)
+    if((t1.Equal(t2)) || (t1.After(t2))){
+        fmt.Println("Yes")
+    }else if(t1.Before(t2)){
+         fmt.Println("No")
     }
 
-    if(b){
-        fmt.Println("Yes")
-    }else{
-        fmt.Println("No")
-    }
 }
